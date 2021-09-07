a473657750afd6048a96ef9edc9193665165fc2e
demoprojectsgsk <90204994+demoprojectsgsk@users.noreply.github.com>
2021-09-07 16:45:22 +0530

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+// Import DB and Faker services
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+		
+		$faker = Faker::create();
+
+    	foreach (range(1,500) as $index) {
+            DB::table('employees')->insert([
+                'firstname' => $faker->firstname,
+                'lastname' => $faker->lastname,
+                'email' => $faker->email,
+                'dob' => $faker->date($format = 'D-m-y', $max = '2010',$min = '1980')
+            ]);
+        }
+    }
+}
